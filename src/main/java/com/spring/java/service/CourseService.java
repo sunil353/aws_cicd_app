9efaedc8ed5bd4ceba3de0825fdca7a3bf09c8a8
Sunil Meher <mehersunil734@gmail.com>
2025-12-04 19:46:08 +0530

    added delete method

--- src/main/java/com/spring/java/service/CourseService.java
+++ src/main/java/com/spring/java/service/CourseService.java
@@ -47,5 +47,10 @@ public class CourseService {
     }
 	
     
-    
+    // Delete a course by id
+    public boolean deleteBaseOnCourse(int id) {
+        return courses
+                .removeIf(course -> course.getCourseID() == id);
+    }
+
 }
