f170ede426b51bdd7518ee48074600b639df8b9b
Sunil Meher <mehersunil734@gmail.com>
2025-12-04 13:29:44 +0530

    changes in class and builspec.yaml

--- src/main/java/com/spring/java/service/CourseService.java
+++ src/main/java/com/spring/java/service/CourseService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import com.spring.java.dto.Course;
