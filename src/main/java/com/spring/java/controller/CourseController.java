a898c73b82a6188ec4916527bc74313822ce4aca
Sunil Meher <mehersunil734@gmail.com>
2025-12-05 17:37:09 +0530

    remove health method

--- src/main/java/com/spring/java/controller/CourseController.java
+++ src/main/java/com/spring/java/controller/CourseController.java
@@ -64,11 +64,9 @@ public class CourseController {
 		}
 	}
 
-	@GetMapping("/health")
-	public String healthCheck() {
-		return "UP";
-	}
-
+	/*
+	 * @GetMapping("/health") public String healthCheck() { return "UP"; }
+	 */
 	@GetMapping("/welcome")
 	public String greetings() {
 		return "Hello Techie , AWS CICD Example working fine !";
